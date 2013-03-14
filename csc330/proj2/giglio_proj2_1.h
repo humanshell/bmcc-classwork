88f83f11a90d9ac3b0b4ac33bac1ae75f3a8cf4b
Dominic Giglio <humanshell@gmail.com>
2013-03-14 10:20:58 -0400

    tweaked struct declaration and added/updated method definitions

--- csc330/proj2/giglio_proj2_1.h
+++ csc330/proj2/giglio_proj2_1.h
@@ -3,23 +3,26 @@
 
 typedef int elem;
 
-typedef struct {
+typedef struct node_s {
   elem data;
-  node *next;
-  node *prev;
+  struct node_s *next;
+  struct node_s *prev;
 } node;
 
 elem length(node*);
 
-void deleteOne(elem);
-void deleteAll(elem);
-void insert(elem);
+void printWelcome();
+void printMenu();
+void deleteOne(elem, node*);
+void deleteAll(elem, node*);
+void insert(elem, node*);
 void sortAscending(node*);
 void sortDescending(node*);
 void makeEmpty(node*);
 
-bool isFull(node*);
-bool isEmpty(node*);
-bool find(elem);
+// success == 1, failure == 0
+int isFull(node*);
+int isEmpty(node*);
+int find(elem, node*);
 
 #endif
