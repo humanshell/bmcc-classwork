3d7a761ed2f085bcb8ac46f578d9747bbd089abc
Dominic Giglio <humanshell@gmail.com>
2013-03-13 21:54:10 -0400

    added definitions to header file

---
+++ csc330/proj2/giglio_proj2_1.h
@@ -0,0 +1,20 @@
+#ifndef GIGLIO_PROJ2_1_H
+#define GIGLIO_PROJ2_1_H
+struct node {
+  int data;
+  node *next;
+  node *prev;
+};
+
+int length(node*);
+
+void deleteOne(int);
+void deleteAll(int);
+void insert(int);
+void makeEmpty(node*);
+
+bool isFull(node*);
+bool isEmpty(node*);
+bool find(int);
+
+#endif
