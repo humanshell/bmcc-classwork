d26310298df8aa4bed9b28e4cd092594a46f5186
Dominic Giglio <humanshell@gmail.com>
2013-03-14 10:20:15 -0400

    started print methods

---
+++ csc330/proj2/giglio_proj2_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "giglio_proj2_1.h"
+
+// print a welcome message when program launches
+void printWelcome() {
+  printf("\nWelcome to Dominic's Circular Doubly Linked List example!\n\
+Use the menu below to experimemt with all the different\n\
+ways to create, alter and destroy linked lists.\n"
+  );
+}
+
+void printMenu() {
+
+}
+
