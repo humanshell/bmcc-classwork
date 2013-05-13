8b461373cde62e1534e900dd0cc1dc91abedcba4
Dominic Giglio <humanshell@gmail.com>
2013-05-13 15:28:43 -0400

    added tests for push and pop

--- csc330/proj3/test.c
+++ csc330/proj3/test.c
@@ -16,16 +16,38 @@ static void test_new_gstack(void) {
   assert(test_gstack->top == NULL);
 }
 
+static void test_gstack_push(void) {
+  gstack_t *test_gstack = new_gstack();
+  gstack_push(test_gstack, 1);
+  assert(test_gstack->size == 1);
+  assert(test_gstack->top->val == 1);
+  gstack_push(test_gstack, 2);
+  assert(test_gstack->size == 2);
+  assert(test_gstack->top->val == 2);
+  gstack_push(test_gstack, 3);
+  assert(test_gstack->size == 3);
+  assert(test_gstack->top->val == 3);
+}
+
+static void test_gstack_pop(void) {
+  gstack_t *test_gstack = new_gstack();
+  gstack_push(test_gstack, 1);
+  gstack_push(test_gstack, 2);
+  gstack_push(test_gstack, 3);
+  gstack_pop(test_gstack);
+  assert(test_gstack->size == 2);
+  assert(test_gstack->top->val == 2);
+  gstack_pop(test_gstack);
+  assert(test_gstack->size == 1);
+  assert(test_gstack->top->val == 1);
+}
+
 int main(int argc, const char *argv[]) {
   printf("\ngstack_t: %ldb\n", sizeof(gstack_t));
   printf("node_t: %ldb\n", sizeof(node_t));
   test(new_gstack);
-  //test(new_list);
-  //test(list_insert);
-  //test(list_remove);
-  //test(list_remove_each);
-  //test(list_remove_all);
-  //test(list_find);
+  test(gstack_push);
+  test(gstack_pop);
   puts("... \x1b[32m100%\x1b[0m\n");
   return 0;
 }
