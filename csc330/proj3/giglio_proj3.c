da4bf75bcf4c52e5700d607fb87ffcd1e5bc166b
Dominic Giglio <humanshell@gmail.com>
2013-05-20 14:34:50 -0400

    starting test program over from scratch

--- csc330/proj3/giglio_proj3.c
+++ csc330/proj3/giglio_proj3.c
@@ -2,47 +2,13 @@
 #include <string.h>
 #include "gstack.h"
 
-// these constants define operator precedence
-#define ADD 1
-#define SUB 1
-#define MUL 2
-#define DIV 2
-
-// global variables for evaluating user's expression
-char c, *expression;
-
 // main function
 int main(void) {
 
-  // allocate memory for expression
-  if (!(expression = (char *) malloc(sizeof(c)))) {
-    printf("Could not allocate memory for your expression!\n");
-    return EXIT_FAILURE;
-  }
-
-  // ask the user for an expression
-  printf("Enter an expression and press return: \n");
-
-  // build a dynamic string from the user's input
-  int i = 0;
-  while ((c = getchar()) != '\n') {
-    if (c != ' ') {
-      expression[i++] = (char) c;
-      expression = realloc(expression, sizeof(expression) + sizeof(c));
-    }
-  }
-  expression[i] = '\0';
-
-  printf("Expression: %s\n", expression);
-
-  // parse and evaluate the user's infix expression
-  for (int i = 0, len = strlen(expression) - 1; i < len; i++) {
-    
-  }
-  
-  // free expression and return
-  free(expression);
-  expression = NULL;
+  // free stacks, expression and return
+  //free(operator_stack);
+  //free(number_stack);
+  //free(expression);
   return 0;
 
 }
